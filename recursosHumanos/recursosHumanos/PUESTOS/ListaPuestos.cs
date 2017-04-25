2e9badec0cea55b995387f54e63f1bbc55237172
Carlos Alatorre <carlos_alatorre_95@hotmail.com>
2017-04-24 21:21:52 -0700

    forms puestos

---
+++ recursosHumanos/recursosHumanos/PUESTOS/ListaPuestos.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace recursosHumanos.PUESTOS
+{
+    public partial class listaPuestos : Form
+    {
+        public listaPuestos()
+        {
+            InitializeComponent();
+        }
+    }
+}
